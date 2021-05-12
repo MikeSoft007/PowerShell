5941699ccbd961f2460a73f387f7514a8199622e
MikeSoft007 <mekpenyong2@gmail.com>
2021-05-12 12:36:00 +0000

    Added Get all MFA ststus for all user

---
+++ AllUserWithMFA.ps1
@@ -0,0 +1,37 @@
+﻿Write-Host "Finding Azure Active Directory Accounts..."
+Connect-MsolService
+$Users = Get-MsolUser -All | ? { $_.UserType -ne "Guest" }
+$Report = [System.Collections.Generic.List[Object]]::new() # Create output file
+Write-Host "Processing" $Users.Count "accounts..." 
+ForEach ($User in $Users) {
+    $MFAMethods = $User.StrongAuthenticationMethods.MethodType
+    $MFAEnforced = $User.StrongAuthenticationRequirements.State
+    $MFAPhone = $User.StrongAuthenticationUserDetails.PhoneNumber
+    $DefaultMFAMethod = ($User.StrongAuthenticationMethods | ? { $_.IsDefault -eq "True" }).MethodType
+    If (($MFAEnforced -eq "Enforced") -or ($MFAEnforced -eq "Enabled")) {
+        Switch ($DefaultMFAMethod) {
+            "OneWaySMS" { $MethodUsed = "One-way SMS" }
+            "TwoWayVoiceMobile" { $MethodUsed = "Phone call verification" }
+            "PhoneAppOTP" { $MethodUsed = "Hardware token or authenticator app" }
+            "PhoneAppNotification" { $MethodUsed = "Authenticator app" }
+        }
+    }
+    Else {
+        $MFAEnforced = "Not Enabled"
+        $MethodUsed = "MFA Not Used" 
+    }
+  
+    $ReportLine = [PSCustomObject] @{
+        User        = $User.UserPrincipalName
+        Name        = $User.DisplayName
+        MFAUsed     = $MFAEnforced
+        MFAMethod   = $MethodUsed 
+        PhoneNumber = $MFAPhone
+    }
+                 
+    $Report.Add($ReportLine) 
+}
+
+Write-Host "Report is inC:\Users\TestLab\desktop\MFAUsers.CSV"
+$Report | Select Name, MFAUsed, MFAMethod, PhoneNumber | Sort Name | Out-GridView
+$Report | Sort Name | Export-CSV -NoTypeInformation C:\Users\TestLab\desktop\MFAUsers.csv
