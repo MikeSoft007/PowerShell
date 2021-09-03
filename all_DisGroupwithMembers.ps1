189068e1c392f2d5dec06cc1b79a18dc99b6e587
MikeSoft <54174323+MikeSoft007@users.noreply.github.com>
2021-09-03 14:53:50 +0100

    Add files via upload

---
+++ all_DisGroupwithMembers.ps1
@@ -0,0 +1,18 @@
+﻿$Groups = Get-DistributionGroup
+$Groups | ForEach-Object {
+$group = $_.Name
+$members = ''
+Get-DistributionGroupMember $group | ForEach-Object {
+        If($members) {
+              $members=$members + ";" + $_.Name
+           } Else {
+              $members=$_.Name
+           }
+  }
+New-Object -TypeName PSObject -Property @{
+      GroupName = $group
+      Members = $members
+      EmailAddress = $_.PrimarySMTPAddress
+     
+     }
+} | Export-CSV "C:\Users\TestLab\Desktop\Distribution-Group-Members.csv" -NoTypeInformation -Encoding UTF8
