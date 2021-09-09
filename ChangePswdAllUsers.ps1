8f8dc5cc285b746ebf9ab84eae444d9052d38eab
MikeSoft <54174323+MikeSoft007@users.noreply.github.com>
2021-09-09 16:32:22 +0100

    Add files via upload
    Uploaded file to change password for specific users in CSV

---
+++ ChangePswdAllUsers.ps1
@@ -0,0 +1,7 @@
+﻿
+
+Import-Csv 'C:\Users\TestLab\Desktop\pswd.csv' | ForEach-Object {
+$upn = $_."UserPrincipalName"
+$tempPwd = $_."password"
+Set-MsolUserPassword -UserPrincipalName $upn –NewPassword $tempPwd -ForceChangePassword $False
+}
